3d788a96ddc40cc93a9e13be1de834f3fa1a235e
Congnandaobei <1915137210@qq.com>
2019-07-09 16:42:53 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
-
+// 这个就是用哪个组件把他引过来
+import {
+  Button,
+  Tabbar,
+  TabbarItem
+} from 'vant'
+Vue
+  .use(Button)
+  .use(Tabbar)
+  .use(TabbarItem)
 Vue.config.productionTip = false
 
 new Vue({
